c5d22d9f44af7081ce3f221b0858eac54d9cee8a
master2080 <master20802@gmail.com>
2020-12-22 17:54:03 +0200

    Made user input validtaion be in one method. Moved all of the logic out of Main.

--- MatrixTrace/Program.cs
+++ MatrixTrace/Program.cs
@@ -5,40 +5,38 @@ namespace MatrixTrace
 {
     public class Program
     {
-        public bool GetUserNumberInput(string text, out int result)
+        public int GetUserNumberInput(string text)
         {
             Console.Write(text + " ");
-            return int.TryParse(Console.ReadLine(), out result);            
-        }
-        public bool PromptTryAgain()
-        {
-            Console.WriteLine("You have entered invalid input, press 1 to try again, or any other key to exit");
-            string input = Console.ReadLine();
 
-            return input == "1";
+            if (int.TryParse(Console.ReadLine(), out int result) && result > 0)
+            {
+                return result;
+            }
+
+            Console.WriteLine("You have entered invalid input, please try again or press ctrl+c to exit");
+            return GetUserNumberInput(text);
         }
-        static void Main()
+
+        public void Start()
         {
-            Program program = new Program();
             int rows, columns;
-
             do
             {
-                program.GetUserNumberInput("Please enter the amount of rows(number must be bigger than zero): ", out rows);
-                program.GetUserNumberInput("Please enter the amount of columns(number must be bigger than zero): ", out columns);
-
-                if (rows < 1 || columns < 1)
-                {
-                    if (!program.PromptTryAgain())
-                        Environment.Exit(0);
-                    else continue;
-                }
+                rows = GetUserNumberInput("Please enter the amount of rows(number must be bigger than zero): ");
+                columns = GetUserNumberInput("Please enter the amount of columns(number must be bigger than zero): ");
 
             } while (rows < 1 || columns < 1);
 
             Matrix matrix = new Matrix(rows, columns);
             Console.WriteLine("The trace sum is: " + matrix.MatrixTraceSum);
             matrix.PrintMatrix(ConsoleColor.Red);
+        }
+
+        static void Main()
+        {
+            Program program = new Program();
+            program.Start();
             Console.ReadKey();
         }
     }
